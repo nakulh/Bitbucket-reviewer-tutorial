52e12caedb25805d34bc5ca458d0ea57535008eb
Nakul Havelia <nakulhavelia@gmail.com>
2023-10-23 13:07:36 +0530

    add queue

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,31 @@
 import { fetchComment, createComment, fetchPullRequest, fetchRawDiff } from "/src/api.jsx";
 import { getReviewForPullRequest } from "/src/ai.jsx";
+import { Queue } from '@forge/events';
+import Resolver from "@forge/resolver";
+const queueName = "aiEvents"
+const queue = new Queue({ key: queueName });
+const resolver = new Resolver();
+
+resolver.define("event-listener", async ({ payload, context }) => {
+	await reviewPullRequest(payload);
+});
+
+export const handler = resolver.getDefinitions();
 
 export async function run(event, context) {
 	if (event.actor.type !== "user") return;
 	console.log(JSON.stringify(event, null, 6));
 	let comment = await fetchComment(event);
 	if (isReviewComment(comment.content.raw)) {
-		console.log("is review comment");
-		await reviewPullRequest(event);
-		return;
+		await queue.push(event);
+  		return;
 	  }
 }
 
 const reviewPullRequest = async (event) => {
 	let pr = await fetchPullRequest(event.pullrequest.id, event.repository.uuid, event.workspace.uuid);
-	console.log("PR");
 	console.log(JSON.stringify(pr, null, 6));
 	let diff = await fetchRawDiff(pr.destination.commit.hash, pr.source.commit.hash, event.repository.uuid, event.workspace.uuid);
-	console.log("diff");
 	console.log(diff);
 	let review = await getReviewForPullRequest(diff);
 	console.log("The review is " + review);
